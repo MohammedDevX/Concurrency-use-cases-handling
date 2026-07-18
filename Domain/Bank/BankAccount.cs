4c173e89e585c47a1a740fafc7986e7c1bee9276
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-07-18 20:08:23 +0100

    Create BankAccount class and BankAccountService who contain Withdraw() method with concurency handling using Interlocker.CompareExchanges()

---
+++ Domain/Bank/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿namespace Concurrency_use_cases.Domain.Bank
+{
+    public class BankAccount(double balance)
+    {
+        private double Balance = balance >= 0 ? balance : throw new ArgumentException("The balance should be higher or equal 0");
+        public ref double BalanceRef => ref Balance; // Get the reference of private field Balance to use it outside this class
+
+        public double _balance
+        {
+            get => Balance;
+            set => Balance = value;
+        }
+    }
+}
---
+++ Domain/Bank/BankAccountService.cs
@@ -0,0 +1,30 @@
+﻿namespace Concurrency_use_cases.Domain.Bank
+{
+    public class BankAccountService
+    {
+        public bool Withdraw(BankAccount bankAccount, double amount)
+        {
+            while (true)
+            {
+                double result;
+                double current = bankAccount._balance;
+                if (current < amount || current == 0 || amount <= 0)
+                {
+                    throw new ArgumentException("Please select amount that are less than balance account and positive");
+                }
+                else
+                {
+                    result = current - amount;
+                }
+
+                if (Interlocked.CompareExchange(ref bankAccount.BalanceRef, result, current) != current)
+                {
+                    
+                } else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
