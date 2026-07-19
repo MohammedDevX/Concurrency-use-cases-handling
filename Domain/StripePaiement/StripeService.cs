4b471852bf903edd243ea765f30e8475c2b1581d
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2026-07-19 15:19:14 +0100

    Add Order, StripeService and PaiementService that contain ProcessPaiement() who manage concurreny use case 2

---
+++ Domain/StripePaiement/StripeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurrency_use_cases.Domain.StripePaiement
+{
+    internal class StripeService
+    {
+    }
+}
